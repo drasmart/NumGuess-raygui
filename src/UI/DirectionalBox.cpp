8b437ef54c45374f26f5af1135a1b3f5a94f4160
Maksym Kucherov <maximus.the0@gmail.com>
2025-02-22 02:12:45 +0200

    Add DirectionalBox.

---
+++ src/UI/DirectionalBox.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Maksym Kucherov on 22-Feb-25.
+//
+
+#include "DirectionalBox.h"
+
+#include "FuncDrawable.h"
+#include "BoxingDrawable.h"
+
+#include "raygui.h"
+
+namespace UI {
+
+    std::unique_ptr<IDrawable> DirectionalBox::toDrawable() const {
+        return std::make_unique<FuncDrawable>([clone = *this](const DrawRequest &drawRequest){
+            std::vector<DrawRequestIdFragment> keys;
+            keys.reserve(clone.count);
+            for (size_t i = 0; i < clone.count; i++) {
+                keys.push_back(clone.keys(i));
+            }
+            float minFitWidth;
+            if (clone.direction.x != 0) {
+                minFitWidth = (
+                (drawRequest.rectangle.width
+                    - (float)(clone.count + 1) * clone.insets.x)
+                    / (float)clone.count
+                );
+            } else {
+                minFitWidth = drawRequest.rectangle.width - 2 * clone.insets.x;
+            }
+            Vector2 maxSize = { 0, 0 };
+            Vector2 fullSize = {
+                (clone.direction.x * ((float)clone.count - 1) + 2) * clone.insets.x,
+                (clone.direction.y * ((float)clone.count - 1) + 2) * clone.insets.y,
+            };
+            for (const auto &key : keys) {
+                const Vector2 nextSize = clone.preferredSizes(key, minFitWidth, drawRequest.rectangle.width);
+                if (maxSize.x < nextSize.x) {
+                    maxSize.x = nextSize.x;
+                }
+                if (maxSize.y < nextSize.y) {
+                    maxSize.y = nextSize.y;
+                }
+                fullSize.x += clone.direction.x * nextSize.x;
+                fullSize.y += clone.direction.y * nextSize.y;
+            }
+            FuncDrawable([&clone, &keys, &fullSize](const DrawRequest &drawRequest2) {
+                Rectangle rect = {
+                    drawRequest2.rectangle.x + clone.insets.x,
+                    drawRequest2.rectangle.y + clone.insets.y,
+                    fullSize.x,
+                    fullSize.y,
+                };
+                for (size_t i = 0; i < clone.count; i++) {
+                    clone.drawers(keys[i], drawRequest2.child(keys[i], rect));
+                    rect.x += clone.direction.x * (clone.insets.x + fullSize.x);
+                    rect.y += clone.direction.y * (clone.insets.y + fullSize.y);
+                }
+            }).scrollable(clone.title, fullSize, clone.offset)->drawAt(drawRequest);
+        });
+    }
+
+} // UI
---
+++ src/UI/DirectionalBox.h
@@ -0,0 +1,31 @@
+//
+// Created by Maksym Kucherov on 22-Feb-25.
+//
+
+#ifndef DIRECTIONALBOX_H
+#define DIRECTIONALBOX_H
+
+#include "IDrawable.h"
+
+#include <memory>
+#include <functional>
+
+namespace UI {
+
+    struct DirectionalBox {
+        const char * const title;
+        const Vector2 direction;
+        Vector2 &offset;
+        const Vector2 insets;
+        const size_t count;
+        const std::function<DrawRequestIdFragment(size_t)> keys;
+        const std::function<Vector2(const DrawRequestIdFragment &, float, float)> preferredSizes;
+        const std::function<void(const DrawRequestIdFragment &, const DrawRequest &)> drawers;
+
+        [[nodiscard]]
+        std::unique_ptr<IDrawable> toDrawable() const;
+    };
+
+} // UI
+
+#endif //DIRECTIONALBOX_H
